0fb8ef42dc591ad9da5f4490d950b178851fb7b2
Mariam Azlina <mriamzlin02@gmail.com>
2023-01-04 00:09:31 +0800

    NEW UPDATESSSSSSSSSSS

--- app/src/main/java/com/example/aotg_v1/Notes.java
+++ app/src/main/java/com/example/aotg_v1/Notes.java
@@ -2,10 +2,8 @@ package com.example.aotg_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+
 
 public class Notes extends AppCompatActivity {
 
@@ -14,11 +12,6 @@ public class Notes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
     }
-    public void openWebLink(View view) {
-        String url = "https://www.youtube.com/watch?v=aI_kBLjWT8E&list=PLTiFz0t_6FnuQUucApbpd00hstaIa6QtH";
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(url));
-        startActivity(intent);
-    }
+
 
 }
